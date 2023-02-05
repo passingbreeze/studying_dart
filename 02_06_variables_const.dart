b91f098a2a43314a98f7221963760c50c86d3aa7
passingbreeze <jungmin1237@pusan.ac.kr>
2023-02-06 00:08:37 +0900

    feat: add variable naming convention

---
+++ 02_06_variables_const.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  // compile-time constant -> const, 컴파일 시점에는 반드시 알아야하는 상수
+  const name = 'hongjeongmin';
+  // late const lastname = 'hong'; -> late 사용할 수 없다.
+}
+
+/**
+ * Variable Naming Convention
+ * 
+ * _, $, alphabet, number only available
+ * its name cannot begin with numbers
+ * 
+ */
