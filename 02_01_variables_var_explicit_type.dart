a4d0a4aa0d9e01fb0f8dfef5b47182d3dd57d756
passingbreeze <jungmin1237@pusan.ac.kr>
2023-02-05 21:59:09 +0900

    feat: dart variables - 1

---
+++ 02_01_variables_var_explicit_type.dart
@@ -0,0 +1,6 @@
+void main() {
+  var name = 'jeongmin'; // local variable in function or class method.
+  name = '정민';
+  String lastname = 'Hong'; // for class variable and property
+  lastname = '홍';
+}
