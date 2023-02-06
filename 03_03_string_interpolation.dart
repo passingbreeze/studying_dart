03223917f196b9013ede94b94c9aaba06d2908a0
passingbreeze <jungmin1237@pusan.ac.kr>
2023-02-07 00:35:27 +0900

    feat: string interploation of dart

---
+++ 03_03_string_interpolation.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  String name = 'Hong Jeongmin';
+  int age = 30;
+  var greeting =
+      "Hello everyone, my name is $name, and I'm ${age + 2}"; // string interpolation
+  print(greeting);
+}
